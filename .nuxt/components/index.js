710fc6f98155ec9b974994cdbe3341cd0633fdba
Pavel Mikheev <pavel.mikheev@t-systems.com>
2021-01-22 05:00:31 +0300

    remove nuxt folder from git

--- .nuxt/components/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Logo } from '../..\\components\\Logo.vue'
-export { default as UserAuthForm } from '../..\\components\\Auth\\Login\\UserAuthForm.vue'
-export { default as UserAuthMixin } from '../..\\components\\Auth\\Login\\UserAuthMixin.js'
-export { default as LogoutButton } from '../..\\components\\Auth\\Logout\\LogoutButton.vue'
-
-export const LazyLogo = import('../..\\components\\Logo.vue' /* webpackChunkName: "components_Logo" */).then(c => c.default || c)
-export const LazyUserAuthForm = import('../..\\components\\Auth\\Login\\UserAuthForm.vue' /* webpackChunkName: "components_Auth/Login/UserAuthForm" */).then(c => c.default || c)
-export const LazyUserAuthMixin = import('../..\\components\\Auth\\Login\\UserAuthMixin.js' /* webpackChunkName: "components_Auth/Login/UserAuthMixin" */).then(c => c.default || c)
-export const LazyLogoutButton = import('../..\\components\\Auth\\Logout\\LogoutButton.vue' /* webpackChunkName: "components_Auth/Logout/LogoutButton" */).then(c => c.default || c)
